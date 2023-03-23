cbab9393c037511d5dccede7a31e501c8707404a
beckho <vngkgk624@gmail.com>
2023-03-23 11:04:10 +0900

    MCP 로직 연동 부분 까지 개발 완료

--- Readme.md
+++ Readme.md
@@ -1,2 +1,9 @@
 # Todo
 EqpInit 로직 업데이트시 MQ명령어로 로직 업데이트 해주기
+
+# How to Add MPFdcApi
+## install
+```
+pip install git+https://github.com/kimbakcho/fdcmp 
+//upgrade
+pip install --upgrade git+https://github.com/kimbakcho/fdcmp
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="fdcmp",
+    version='0.0.1',
+    description="Fdc Message Parser",
+    author="Fdc",
+    author_email="XXXX@sfac.co.kr",
+    packages=["bFdc"],
+    install_requires=[],
+)
