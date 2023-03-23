c9f8a5cc1945594a1a28270d973774bab3432242
beckho <vngkgk624@gmail.com>
2023-03-23 14:10:11 +0900

    라이브러리 정리 작업

--- mpl/apps.py
+++ mpl/apps.py
@@ -10,6 +10,7 @@ from mpl.Listener.MPLCommandListener import MPLCommandListener
 from mpl.Listener.MPListener import MPListener
 from mpl.Process.MPLProcessWorker import mplProcessWorker
 
+from fdcmp.settings import env
 
 class MplConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
@@ -18,8 +19,6 @@ class MplConfig(AppConfig):
     def ready(self):
         if 'runserver' not in sys.argv:
             return True
-        env = environ.Env()
-
         r = requests.get(f"{env('BFDC_URL')}/mp/core/{env('MP_CORE_ID')}/")
 
         coreInfo = r.json()
--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name="fdcmp",
-    version='0.0.5',
+    version='0.0.6',
     description="Fdc Message Parser",
     author="Fdc",
     author_email="XXXX@sfac.co.kr",
-    packages=["bFdcAPI.Eqp", "bFdcAPI.MCP", "bFdcAPI.MP", "bFdcAPI", "FDCContext"],
+    packages=find_packages(where='fdcmp', include=['bFdcAPI*', 'FDCContext']),
     install_requires=["django-environ"],
 )
