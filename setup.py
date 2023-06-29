92fd33050c8db48b89f817cc406fdfba1c7f2ab5
beckho <vngkgk624@gmail.com>
2023-06-30 08:59:01 +0900

    capa Context 에 eqp code name module code name 추가

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="fdcmp",
-    version='0.0.75',
+    version='0.0.76',
     description="Fdc Message Parser",
     author="Fdc",
     author_email="XXXX@sfac.co.kr",
