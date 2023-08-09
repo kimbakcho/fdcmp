c0d59718895e12ce15a20f8bf17afac3ca52d7ba
beckho <vngkgk624@gmail.com>
2023-08-09 12:32:57 +0900

    setup.py 수정

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup, find_packages
 
 setup(
     name="fdcmp",
-    version='0.0.95',
+    version='0.0.96',
     description="Fdc Message Parser",
     author="Fdc",
     author_email="XXXX@sfac.co.kr",
     packages=find_packages(include=['bFdcAPI*', 'FDCContext', 'ESB*','CapaContext*','ACPContext*']),
-    install_requires=["django-environ", "requests", "stomp.py", "pymongo==3.12.1"],
+    install_requires=["django-environ", "requests", "stomp.py"],
 )
