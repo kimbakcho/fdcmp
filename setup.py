9052e3367d2a34bd425e752ff3bcc0174959dc20
beckho <vngkgk624@gmail.com>
2023-09-06 12:50:21 +0900

    cycleTime Manager recipe=>paramName, cycleTime=>weight 로 이름 변경

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="fdcmp",
-    version='0.0.109',
+    version='0.0.110',
     description="Fdc Message Parser",
     author="Fdc",
     author_email="XXXX@sfac.co.kr",
