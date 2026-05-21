ed00b9d0516c27bec713e2bf258b5e16eb49532c
bhkim <vngkgk624@naver.com>
2026-05-21 14:34:53 +0900

    saveTrace 가 있을 경우만 저장

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="fdcmp",
-    version='0.0.140',
+    version='0.0.141',
     description="Fdc Message Parser",
     author="Fdc",
     author_email="XXXX@sfac.co.kr",
